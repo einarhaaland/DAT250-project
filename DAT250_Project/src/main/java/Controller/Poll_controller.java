ec081909efbb586cc5e7e7050fdc6bef824301eb
Michal J <michaljuzyszyn@gmail.com>
2021-09-19 14:16:29 +0200

    add Poll DAO controller

--- DAT250_Project/src/main/java/Controller/Poll_controller.java
+++ DAT250_Project/src/main/java/Controller/Poll_controller.java
@@ -1,4 +1,59 @@
 package Controller;
 
+import Model.Poll;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+
 public class Poll_controller {
+
+    private static List<Poll> pollList;
+    private EntityManagerFactory factory;
+    private void getFactory(EntityManagerFactory fac) {
+        factory = fac;
+    }
+
+    private EntityManager entityManager = factory.createEntityManager();
+
+    public Optional<Poll> get(int id) {
+        return Optional.ofNullable(entityManager.find(Poll.class, id));
+    }
+
+
+    public List<Poll> getAll() {
+        Query query = entityManager.createQuery("SELECT p FROM Poll p");
+        return query.getResultList();
+    }
+
+
+    public void save(Poll poll) {
+        executeInsideTransaction(entityManager -> entityManager.persist(poll));
+    }
+
+
+    public void update(Poll poll, String params) {
+        poll.setQuestion(Objects.requireNonNull(params, "Question cannot be null."));
+        executeInsideTransaction(entityManager -> entityManager.merge(poll));
+    }
+
+
+    public void delete(Poll poll) {
+        executeInsideTransaction(entityManager -> entityManager.remove(poll));
+    }
+
+    private void executeInsideTransaction(Consumer<EntityManager> action) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager);
+            tx.commit();
+        }
+        catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        }
+    }
 }
