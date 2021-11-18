f19c731d7d7defb08b1cf760625b06e2a02b3d03
Sondre <Sondre.Gjesdal@gmail.com>
2021-11-18 15:52:23 +0100

    dweet messaging

--- DAT250_Project/src/main/java/dat250_frontend/src/components/Polls/CreatePoll.js
+++ DAT250_Project/src/main/java/dat250_frontend/src/components/Polls/CreatePoll.js
@@ -8,14 +8,14 @@ export default function CreatePoll(props) {
     const handlePollResults = (pollId) => {
         //send result to dweet.io
         //To dweet from your thing, simply call a URL like:https://dweet.io/dweet/for/my-thing-name?hello=world
-            //Just replace my-thing-name with a unique name. That's it!
+        //Just replace my-thing-name with a unique name. That's it!
         //To read the latest dweet for a thing, you can call...https://dweet.io/get/dweets/for/my-thing-name
         let yesVotes = 0;
         let noVotes = 0;
 
         fetch(`/polls/${pollId}/votes`)
             .then((response) => response.json())
-            .then((data) => {  
+            .then((data) => {
                 for (var i = 0; i < data.length; i++) {
                     console.log("inni loop");
                     if (data[i].vote === "YES") yesVotes++;
@@ -27,9 +27,20 @@ export default function CreatePoll(props) {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
-                        question : question,
-                        yes : yesVotes,
-                        no : noVotes
+                        question: question,
+                        yes: yesVotes,
+                        no: noVotes
+                    })
+                })
+            })
+            .then(() => {
+                fetch("https://dweet.io/dweet/for/dat250poller?" + question, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        question: question,
+                        yes: yesVotes,
+                        no: noVotes
                     })
                 })
             })
@@ -44,26 +55,36 @@ export default function CreatePoll(props) {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
-            body: JSON.stringify({question: question})
+            body: JSON.stringify({ question: question })
         })
             .then(resp => resp.json())
             .then((data) => {
-                setTimeout( function() { handlePollResults(data.id); }, 1000*40); //time in ms
+                setTimeout(function () { handlePollResults(data.id); }, 1000 * 40); //time in ms
             })
+
+        fetch("https://dweet.io/dweet/for/dat250poller?" + question, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                question: question,
+                
+            })
+        })
+
     }
-    
+
     return (
         <div>
             <div>
                 <h2>Create new poll</h2>
                 <form>
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         name="question"
-                        placeholder="Poll Question" 
-                        value= {question}
+                        placeholder="Poll Question"
+                        value={question}
                         onChange={(e) => setQuestion(e.target.value)}
-                        />
+                    />
                     <button type="button" onClick={handleSubmit}>Submit</button>
                 </form>
             </div>
