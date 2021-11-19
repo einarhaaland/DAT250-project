ff97187d493b3d5b1d2dfbf3539ddcbd1bec6ebf
Daniel Lundegaard Jacobsen <djdan30@gmail.com>
2021-11-19 10:50:01 +0100

    Password is now hashed and salted

--- DAT250_Project/src/main/java/Controller/JpaPollUserDao.java
+++ DAT250_Project/src/main/java/Controller/JpaPollUserDao.java
@@ -3,10 +3,13 @@ package Controller;
 import Model.PollUser;
 import Test.JPATest;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.persistence.*;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.*;
 import java.util.function.Consumer;
 
 import static Globals.Globals.PERSISTENCE_UNIT_NAME;
@@ -16,13 +19,16 @@ public class JpaPollUserDao {
     private EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
     private EntityManager em = factory.createEntityManager();
 
+    // generates the Salt value. It can be stored in a database.
+    String saltvalue = PassBasedEnc.getSaltvalue(30);
+
     //Login
     public PollUser login(PollUser user) {
 
         List<PollUser> list = getAll();
 
         for (PollUser p : list) {
-            if (p.getUsername().equals(user.getUsername()) && p.getPassword().equals(user.getPassword())) {
+            if (p.getUsername().equals(user.getUsername()) && PassBasedEnc.verifyUserPassword(user.getPassword(),p.getPassword(),saltvalue)) {
                 return get(p.getId());
             }
         }
@@ -47,6 +53,11 @@ public class JpaPollUserDao {
                 throw new IllegalStateException("User already exists");
             }
         }
+
+        // Password-Based Encryption using Salt and Base64
+        String encryptedPassword = PassBasedEnc.generateSecurePassword(user.getPassword(), saltvalue);
+
+        user.setPassword(encryptedPassword);
 
         executeInsideTransaction(em -> em.persist(user));
     }
@@ -77,5 +88,74 @@ public class JpaPollUserDao {
             tx.rollback();
             throw e;
         }
+    }
+}
+
+class PassBasedEnc
+{
+    /* Declaration of variables */
+    private static final Random random = new SecureRandom();
+    private static final String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int iterations = 10000;
+    private static final int keylength = 256;
+
+    /* Method to generate the salt value. */
+    public static String getSaltvalue(int length)
+    {
+        StringBuilder finalval = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            finalval.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return new String(finalval);
+    }
+
+    /* Method to generate the hash value */
+    public static byte[] hash(char[] password, byte[] salt)
+    {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, keylength);
+        Arrays.fill(password, Character.MIN_VALUE);
+        try
+        {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return skf.generateSecret(spec).getEncoded();
+        }
+        catch (NoSuchAlgorithmException | InvalidKeySpecException e)
+        {
+            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+        }
+        finally
+        {
+            spec.clearPassword();
+        }
+    }
+
+    /* Method to encrypt the password using the original password and salt value. */
+    public static String generateSecurePassword(String password, String salt)
+    {
+        String finalval = null;
+
+        byte[] securePassword = hash(password.toCharArray(), salt.getBytes());
+
+        finalval = Base64.getEncoder().encodeToString(securePassword);
+
+        return finalval;
+    }
+
+    /* Method to verify if both password matches or not */
+    public static boolean verifyUserPassword(String providedPassword,
+                                             String securedPassword, String salt)
+    {
+        boolean finalval = false;
+
+        /* Generate New secure password with the same salt */
+        String newSecurePassword = generateSecurePassword(providedPassword, salt);
+
+        /* Check if two passwords are equal */
+        finalval = newSecurePassword.equalsIgnoreCase(securedPassword);
+
+        return finalval;
     }
 }
